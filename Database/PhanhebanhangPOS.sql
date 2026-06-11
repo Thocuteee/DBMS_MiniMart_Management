3285f7bbfce66ff23656a8e8748bfed5005fb1f4
Thocuteee <thoch3765@ut.edu.vn>
2026-06-11 19:30:05 +0700

    update ACID for POS

--- Database/PhanhebanhangPOS.sql
+++ Database/PhanhebanhangPOS.sql
@@ -88,3 +88,90 @@ SELECT * FROM HOADON WHERE MaHD = 'HD_TEST_01';        -- TongTien và ThanhTien
 SELECT * FROM ChiTietHoaDon WHERE MaHD = 'HD_TEST_01'; -- Xuất hiện 1 dòng SP01
 
 
+
+-- ACID
+-- 1. Tính nguyên tử (ATOMICITY)
+BEGIN TRY
+    BEGIN TRAN;
+
+    INSERT INTO HOADON (MaHD, NgayLap, MaNV, MaKH, TongTien, GiamGia, ThanhTien)
+    VALUES ('HD_ATOMIC_99', GETDATE(), 'NV01', 'KH01', 0, 0, 0);
+
+    SELECT 1 / 0; 
+
+    UPDATE TonKho SET SoLuongTonKho = SoLuongTonKho - 2 WHERE MaKho = 'K02' AND MaSP = 'SP01';
+
+    COMMIT TRAN;
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0 ROLLBACK TRAN; 
+    PRINT N'Đã xảy ra lỗi! Hệ thống tự động ROLLBACK sạch sẽ, không để lại rác.';
+END CATCH;
+GO
+
+
+-- KIỂM TRA: Bảng HoaDon vẫn sạch sẽ, không có mã 'HD_ATOMIC_99'
+SELECT * FROM HOADON WHERE MaHD = 'HD_ATOMIC_99';
+
+
+--2. Tính nhất quán(CONSISTENCY)
+-- Bạn cố tình chạy lệnh này với mã nhân viên không tồn tại
+DECLARE @MaNV VARCHAR(10) = 'NV_FAKE';
+
+IF NOT EXISTS (SELECT 1 FROM NhanVien WHERE MaNV = @MaNV)
+BEGIN
+    RAISERROR(N'Lỗi Nhất Quán: Nhân viên này không có trong hệ thống siêu thị!', 16, 1);
+END
+ELSE
+BEGIN
+    INSERT INTO HOADON (MaHD, MaNV) VALUES ('HD_TEST_02', @MaNV);
+END
+
+-- KIỂM TRA:
+SELECT * FROM HOADON WHERE MaHD = 'HD_TEST_02';
+
+
+--Tính cô lập(ISOLATION)
+-- LƯU Ý: 2 TAB 
+--Tab1:
+UPDATE TonKho 
+SET SoLuongTonKho = 50 
+WHERE MaKho = 'K02' AND MaSP = 'SP01';
+GO
+
+-- CÁCH KHẮC PHỤC: Thêm dòng lệnh khóa cô lập này vào đầu!
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+
+BEGIN TRAN;
+DECLARE @Ton INT;
+SELECT @Ton = SoLuongTonKho FROM TonKho WHERE MaKho = 'K02' AND MaSP = 'SP01'; -- Đọc ra 50
+
+WAITFOR DELAY '00:00:06'; 
+
+UPDATE TonKho SET SoLuongTonKho = @Ton - 2 WHERE MaKho = 'K02' AND MaSP = 'SP01';
+COMMIT TRAN;
+
+
+--Tab2:
+-- CÁCH KHẮC PHỤC: Thêm dòng lệnh khóa cô lập này vào đầu!
+-- SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+
+BEGIN TRAN;
+DECLARE @Ton INT;
+SELECT @Ton = SoLuongTonKho FROM TonKho WHERE MaKho = 'K02' AND MaSP = 'SP01'; 
+
+UPDATE TonKho SET SoLuongTonKho = @Ton - 5 WHERE MaKho = 'K02' AND MaSP = 'SP01';
+COMMIT TRAN;
+
+-- Kết quả:
+SELECT * FROM TonKho WHERE MaKho = 'K02' AND MaSP = 'SP01';
+
+
+
+-- Tính bền vững(Durability)
+EXEC sp_GiaoTacBanHang     
+    @MaHD = 'HD_DURABLE_99', @MaNV = 'NV01', @MaKH = 'KH01', 
+    @MaKho = 'K02', @MaSP = 'SP01', @SoLuong = 1, @DonGiaBan = 50000;
+
+-- Kq:
+SELECT * FROM HOADON WHERE MaHD = 'HD_DURABLE_99';
